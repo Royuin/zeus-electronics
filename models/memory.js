9574f55c3510e0e91eecd817c6bdbdd8606616ad
Gage <77414756+Royuin@users.noreply.github.com>
2024-01-13 09:52:19 -0600

    Made a model schema for memory products with a virtual url

---
+++ models/memory.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const MemorySchema = new Schema ({
+  name: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Memory', required: true },
+  description: { type: String, required: True },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+MemorySchema.virtual('url').get(function() {
+  return `/memory/${this._id}`;
+});
+
+module.exports = mongoose.model('Memory', MemorySchema);
