bdd6293013183ccc3b0402323c7aeba1f2a99ee5
Gage <77414756+Royuin@users.noreply.github.com>
2024-01-13 09:11:28 -0600

    Made a model schema for processor products

---
+++ models/processor.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const ProcessorSchema = new Schema ({
+  name: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required : true },
+  description: { type: String, required: true },
+  price: { type: Number, required : true },
+  quantity: { type: Number, required: true },
+})
+
+ProcessorSchema.virtual('url').get(function() {
+  return `/processors/${this._id}`;
+});
+
+module.exports = mongoose.model('Processor', ProcessorSchema);
