1b3b68db0bff918894af21778e4ef666f36fcfa2
Gage <77414756+Royuin@users.noreply.github.com>
2024-01-18 22:23:21 -0600

    Fixed true typo

--- models/memory.js
+++ models/memory.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const MemorySchema = new Schema ({
   name: { type: String, required: true },
   category: { type: Schema.Types.ObjectId, ref: 'Memory', required: true },
-  description: { type: String, required: True },
+  description: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
 });
