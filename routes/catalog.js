15dc64d69f83d0ac6e01787a1bfac95fb9becc89
Gage <77414756+Royuin@users.noreply.github.com>
2024-01-21 19:53:26 -0600

    Added router for graphics-cards category list

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,4 +4,6 @@ const categoryController = require('../controllers/categoryController');
 
 router.get('/', categoryController.category_list);
 
+router.get('/category/graphics-cards/:id', categoryController.graphics_cards_list);
+
 module.exports = router;
