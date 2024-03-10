a7c8eee7adcee974092df44c2e78f8b4c93f3cac
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-10 17:58:55 -0500

    Added route for memory product details with product id

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,10 +6,10 @@ const graphicsCardController = require('../controllers/graphicsCardController');
 router.get('/', categoryController.category_list);
 
 router.get('/category/graphics-cards/:id', categoryController.graphics_cards_list);
-
 router.get('/category/graphics-cards/product/:productId', graphicsCardController.graphics_card_details);
 
 router.get('/category/memory/:id', categoryController.memory_list);
+router.get('/category/memory/product/:productId', categoryController.memory_product_details);
 
 router.get('/category/motherboards/:id', categoryController.motherboard_list);
 
