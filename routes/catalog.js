e4c823187102065211ed35cc22e5ae8e7860d410
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-10 19:16:09 -0500

    Imported memoryController and made memory_product_details method be called from memoryController instead of categoryController

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const graphicsCardController = require('../controllers/graphicsCardController');
+const memoryController = require('../controllers/memoryController');
 
 router.get('/', categoryController.category_list);
 
@@ -9,7 +10,7 @@ router.get('/category/graphics-cards/:id', categoryController.graphics_cards_lis
 router.get('/category/graphics-cards/product/:productId', graphicsCardController.graphics_card_details);
 
 router.get('/category/memory/:id', categoryController.memory_list);
-router.get('/category/memory/product/:productId', categoryController.memory_product_details);
+router.get('/category/memory/product/:productId', memoryController.memory_product_details);
 
 router.get('/category/motherboards/:id', categoryController.motherboard_list);
 
