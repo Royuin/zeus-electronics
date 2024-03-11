c3dd0180657f6ab3ca664aaeae4b5ac329cb5e0c
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-10 19:36:39 -0500

    Made route for a specific motherboard with its id as a parameter

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const graphicsCardController = require('../controllers/graphicsCardController');
 const memoryController = require('../controllers/memoryController');
+const motherboardController = require('../controllers/motherboardController');
 
 router.get('/', categoryController.category_list);
 
@@ -13,6 +14,7 @@ router.get('/category/memory/:id', categoryController.memory_list);
 router.get('/category/memory/product/:productId', memoryController.memory_product_details);
 
 router.get('/category/motherboards/:id', categoryController.motherboard_list);
+router.get('/category/motherboards/product/:productId', motherboardController.motherboard_details);
 
 router.get('/category/processors/:id', categoryController.processor_list);
 
