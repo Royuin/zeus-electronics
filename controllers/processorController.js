1d3e386c346291737b23e5916313242ac0c6e28d
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-11 20:29:44 -0500

    Made method for getting a given processor product from id in url and rendering view file with it

---
+++ controllers/processorController.js
@@ -0,0 +1,11 @@
+const Processor = require('../models/processor');
+const asyncHandler = require('express-async-handler');
+
+exports.processor_details = asyncHandler( async (req, res, next) => {
+  const processorProduct = await Processor.findById(req.productId).exec();
+
+  res.render('processor_details', {
+    title: processorProduct.name,
+    product: processorProduct,
+    });
+});
