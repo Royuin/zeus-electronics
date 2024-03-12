37273c2e34fe29a5f5ad0ac20943af9f5d4afbd8
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-11 20:45:39 -0500

    Fixed promise for fetching processor product not using params of url

--- controllers/processorController.js
+++ controllers/processorController.js
@@ -2,7 +2,7 @@ const Processor = require('../models/processor');
 const asyncHandler = require('express-async-handler');
 
 exports.processor_details = asyncHandler( async (req, res, next) => {
-  const processorProduct = await Processor.findById(req.productId).exec();
+  const processorProduct = await Processor.findById(req.params.productId).exec();
 
   res.render('processor_details', {
     title: processorProduct.name,
