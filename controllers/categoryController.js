889dac0a25697dc50ade94089bc674e81239e933
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-17 10:14:32 -0500

    Passed graphics card category id to list view file so it can be used in url navigation with link to product creation page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -52,9 +52,12 @@ exports.category_list = asyncHandler(async (req, res, next) => {
 
 exports.graphics_cards_list = asyncHandler( async (req, res, next) => {
   const allGraphicsCards = await GraphicsCard.find().sort({ name: 1 }).exec();
+  const graphicsCardCategory = await Category.findById(req.params.id).exec();
+  const categoryId = graphicsCardCategory._id;
   res.render('graphics_cards_list', {
     title: 'Graphics Cards',
     graphics_cards_list: allGraphicsCards,
+    categoryId: categoryId,
   })
 })
 
