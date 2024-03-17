224d4acc5984139f99c4c9e6493a67dd04d6ab53
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-17 15:24:40 -0500

    Added router for post requests on creating new graphics card products

--- routes/catalog.js
+++ routes/catalog.js
@@ -14,6 +14,7 @@ router.post('/category/create', categoryController.category_create_post);
 router.get('/category/graphics-cards/:id', categoryController.graphics_cards_list);
 router.get('/category/graphics-cards/product/:productId', graphicsCardController.graphics_card_details);
 router.get('/category/graphics-cards/:id/create', graphicsCardController.graphics_card_create_get);
+router.post('/category/graphics-cards/:id/create', graphicsCardController.graphics_card_create_post);
 
 router.get('/category/memory/:id', categoryController.memory_list);
 router.get('/category/memory/product/:productId', memoryController.memory_product_details);
