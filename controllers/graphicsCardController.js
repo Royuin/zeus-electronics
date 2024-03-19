8d34f3abc6866378679292acafe4b6f84475f9a8
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-18 22:03:37 -0500

    Moved methods for creating a new product and error handling to product controller instead of graphics card controller to prevent repetition of forms with different product types

--- controllers/graphicsCardController.js
+++ controllers/graphicsCardController.js
@@ -10,33 +10,3 @@ exports.graphics_card_details = asyncHandler( async (req, res, next) => {
     graphicsCard: graphicsCard,
   });
 });
-
-exports.graphics_card_create_get = asyncHandler( async (req, res, next) => {
-  res.render('graphics_card_form', {
-    title: 'Create a new graphics Card',
-  });
-});
-
-exports.graphics_card_create_post = [
-  body('name', 'Name must contain at least 3 characters.').trim().isLength({min: 3}).escape(),
-  body('description').trim().escape(),
-  body('price', 'Price must not be empty and greater than 0.').trim().isFloat({min: 1}).escape(),
-  body('quantity', 'Quantity must be empty and a whole non negative number.').trim().isInt({min: 0}).escape(),
-
-  asyncHandler( async (req, res, next) => {
-    const errors = validationResult(req);
-    const graphicsCard = new GraphicsCard({name: req.body.name, description: req.body.description, price: req.body.price, quantity: req.body.quantity});
-
-    if(!errors.isEmpty()) {
-      res.render('graphics_card_form', {
-        title: 'Create a new graphics card',
-        errors: errors.array(),
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        quantity: req.body.quantity,
-      });
-      return;
-    }  
-  }),
-]
---
+++ controllers/productController.js
@@ -0,0 +1,31 @@
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+
+exports.product_create_get = asyncHandler( async (req, res, next) => {
+  res.render('product_form', {
+    title: 'Create a new product',
+  });
+});
+
+exports.product_create_post = [
+  body('name', 'Name must contain at least 3 characters.').trim().isLength({min: 3}).escape(),
+  body('description').trim().escape(),
+  body('price', 'Price must not be empty and greater than 0.').trim().isFloat({min: 1}).escape(),
+  body('quantity', 'Quantity must be empty and a whole non negative number.').trim().isInt({min: 0}).escape(),
+
+  asyncHandler( async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      res.render('product_form', {
+        title: 'Create a new product',
+        errors: errors.array(),
+        previous_name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        quantity: req.body.quantity,
+      });
+      return;
+    }  
+  }),
+]
