44f45e48147800410f82c98fe4659346b276c101
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-24 16:29:48 -0500

    Removed router for each category of product and replaced with 1 route that will display any category by id

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
-const graphicsCardController = require('../controllers/graphicsCardController');
-const memoryController = require('../controllers/memoryController');
-const motherboardController = require('../controllers/motherboardController');
-const processorController = require('../controllers/processorController');
 const productController = require('../controllers/productController');
 
 router.get('/', categoryController.category_list);
@@ -12,17 +8,7 @@ router.get('/', categoryController.category_list);
 router.get('/category/create', categoryController.category_create_get);
 router.post('/category/create', categoryController.category_create_post);
 
-router.get('/category/graphics-cards/:id', categoryController.graphics_cards_list);
-router.get('/category/graphics-cards/product/:productId', graphicsCardController.graphics_card_details);
-
-router.get('/category/memory/:id', categoryController.memory_list);
-router.get('/category/memory/product/:productId', memoryController.memory_product_details);
-
-router.get('/category/motherboards/:id', categoryController.motherboard_list);
-router.get('/category/motherboards/product/:productId', motherboardController.motherboard_details);
-
-router.get('/category/processors/:id', categoryController.processor_list);
-router.get('/category/processors/product/:productId', processorController.processor_details);
+router.get('/category/:hypenName/:id', categoryController.category_products);
 
 router.get('/product/create', productController.product_create_get);
 router.post('/product/create', productController.product_create_post);
