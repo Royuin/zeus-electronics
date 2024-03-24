b50b5544fb309ceaeb40c766cd1f1174fc869624
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-24 16:23:29 -0500

    Removed Type from type for category value

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const ProductSchema = new Schema ({
   name: { type: String, required: true },
-  category: {type: Schema.Types.ObjectId, ref: 'Category', required : true },
+  category: {type: Schema.ObjectId, ref: 'Category', required : true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
