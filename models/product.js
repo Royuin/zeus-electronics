76e2f270b80c5d6de65c226789bad8f47c5ea5de
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-23 22:37:37 -0500

    Added a product model that will replace graphics card, processor, memory, and motherboard models. As there is no reason to have seperate models for all of them

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema ({
+  name: { type: String, required: true },
+  category: {type: Schema.Types.ObjectId, ref: 'Category', required : true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+ProductSchema.virtual('url').get(function() {
+  return `product/${this._id}`;
+  });
+
+module.exports = mongoose.model('Product', ProductSchema);
