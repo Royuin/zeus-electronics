aeac00f721018e80169849da72da4e53a0b1cc72
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-25 20:40:40 -0500

    Added routers for handling get and post of category deletion

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,12 +7,12 @@ router.get('/', categoryController.category_list);
 
 router.get('/category/create', categoryController.category_create_get);
 router.post('/category/create', categoryController.category_create_post);
+router.get('/category/:hypenName/:id', categoryController.category_products);
+router.get('/category/:hypenName/:id/delete', categoryController.category_delete_get);
+router.get('/category/:hypenName/:id/delete', categoryController.category_delete_post);
 
 router.get('/product/create', productController.product_create_get);
 router.post('/product/create', productController.product_create_post);
-
-router.get('/category/:hypenName/:id', categoryController.category_products);
-
 router.get('/product/:id', productController.product_details);
 router.get('/product/:id/delete', productController.product_delete_get);
 router.post('/product/:id/delete', productController.product_delete_post);
