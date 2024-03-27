e786bb1bf603a8d3cd607f13b1007cd13cfd55c4
Gage <77414756+Royuin@users.noreply.github.com>
2024-03-26 20:36:32 -0500

    Changed a get route to a post route that was calling a post function for deleting a category

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,7 +9,7 @@ router.get('/category/create', categoryController.category_create_get);
 router.post('/category/create', categoryController.category_create_post);
 router.get('/category/:hypenName/:id', categoryController.category_products);
 router.get('/category/:hypenName/:id/delete', categoryController.category_delete_get);
-router.get('/category/:hypenName/:id/delete', categoryController.category_delete_post);
+router.post('/category/:hypenName/:id/delete', categoryController.category_delete_post);
 
 router.get('/product/create', productController.product_create_get);
 router.post('/product/create', productController.product_create_post);
